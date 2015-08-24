194b78d9ad6be99624a223427822d285f26ba18c
yohanstra <jreinstra@gmail.com>
2015-08-24 14:42:21 -0700

    Created app pathfinder with Article model

---
+++ pathfinder/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(unique=True, max_length=150)),
+                ('linked_articles', models.ManyToManyField(to='pathfinder.Article')),
+            ],
+        ),
+    ]
